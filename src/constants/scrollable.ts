f5f3bf25e9c82c74260611707112d5ce5571bafe
adithyavis <adithyaviswam@gmail.com>
2025-04-04 10:30:52 +0530

    feat: change deceleration rate

--- src/constants/scrollable.ts
+++ src/constants/scrollable.ts
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 
 export const DECELERATION_RATE_FOR_SCROLLVIEW = Platform.select({
   ios: 0.9998,
-  android: 0.985,
+  android: 0.9985,
   default: 1,
 });
 
